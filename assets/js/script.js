18cf6739c1a4b3496cc0444b4cee2cd927bcef43
Shatikka McKnight <32470118+shatikka-mcknight@users.noreply.github.com>
2021-03-27 20:40:31 -0400

    setup variables using 'let' and wrote script to display weather data within HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 let appId = "ccd48ccf113eeb3d3f3a46d2f1e3ed7f";
-let degreeDisplay = "metric";
+let units = "metric";
 let searchMethod = "zip";
 
 //create a function to determine what data is being enter for the #searchData zip code or City name, state code and country code
@@ -24,7 +24,7 @@ function searchWeather(searchInfo) {
     searchInfo that includes the Api Key with how to display the degree...
     waiting for the server results and using json to read the information 
     that return show me the information returned*/
-    fetch(`https://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchInfo}&APPID=${appId}&degree=${degreeDisplay}`).then(result => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchInfo}&APPID=${appId}&degree=${units}`).then(result => {
         return result.json();
     }).then(results => {
         init(results);
@@ -34,14 +34,44 @@ function searchWeather(searchInfo) {
 
 function init(resultFromOpenWeather) {
     console.log(resultFromOpenWeather);
+    var cityNameSearch = document.getElementById("cityName")
+    var weatherIconResults = document.getElementById("weatherIcon")
+    var weatherDescriptionResults = document.getElementById("weatherDescription");
+    var tempDegreeResults = document.getElementById("tempDegree");
+    var humityPercentResults = document.getElementById("humityPercent");
+    var windSpeedResults = document.getElementById("windSpeed");
+    var uvIndexResults = document.getElementById("uvIndex");
+
+    weatherIconResults.src = " http://openweathermap.org/img/wn/" + resultFromOpenWeather.weather[0].icon + ".png";
+
+    let resultWeatherDisplay = resultFromOpenWeather.weather[0].description;
+    let resultWeatherArea = resultFromOpenWeather.name;
+    let resultWeatherMain = resultFromOpenWeather.main;
+    let resultWeatherWind = resultFromOpenWeather.wind;
+
+    // capitalWords.join(" ");
+
+    cityNameSearch.innerHTML = "Location: " + resultWeatherArea;
+    weatherDescriptionResults.innerText = "Description: "
+        + resultWeatherDisplay;
     
+
+    tempDegreeResults.innerHTML = "Temp: "
+        + "<li> Current: " + Math.floor(resultWeatherMain.temp) + "&#176</li>"
+        + "<li> High: " + Math.floor(resultWeatherMain.temp_max) + "&#176</li>"
+        + "<li> Low: " + Math.floor(resultWeatherMain.temp_min) + "&#176</li>";
+    
+    humityPercentResults.innerHTML = "Humidity: " + Math.floor(resultWeatherMain.humidity);
+    
+    windSpeedResults.innerHTML = "Wind Speed: " + Math.floor(resultWeatherWind.speed);
 }
+    // console.log();
+
 
 //Setting up the search button interaction
-document.getElementById("submitCity").addEventListener("click", () => {
-    let searchInfo = document.getElementById("searchData").value;
-    //if user input data into the #searchData the OpenWeatherApp will use the information from the searchInfo value enter to procees the search information 
-    if (searchInfo) 
-        searchWeather(searchInfo);
+    document.getElementById("submitCity").addEventListener("click", () => {
+        let searchInfo = document.getElementById("searchData").value;
+        //if user input data into the #searchData the OpenWeatherApp will use the information from the searchInfo value enter to procees the search information 
+        if (searchInfo)
+            searchWeather(searchInfo);
     })
-    
